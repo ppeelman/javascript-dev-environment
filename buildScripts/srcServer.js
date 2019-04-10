ebd79a580cb05b439dee971eef53ff9756799b9d
Philippe Peelman <philippe.peelman@gmail.com>
2019-04-10 14:55:54 +0200

    Babel config added

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,10 @@
-var express = require("express");
-var path = require("path");
-var open = require("open");
+import express from "express";
+import path from "path";
+import open from "open";
+import chalk from "chalk";
 
-var port = 3000;
-var app = express();
+const port = 3000;
+const app = express();
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../src/index.html"));
@@ -11,8 +12,10 @@ app.get("/", function(req, res) {
 
 app.listen(port, function(err) {
   if (err) {
+    console.log(chalk.red("Something went wrong:"));
     console.log(err);
   } else {
+    console.log(chalk.green("Starting app in dev mode..."));
     open("http://localhost:" + port);
   }
 });
