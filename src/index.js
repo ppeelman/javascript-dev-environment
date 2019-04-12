c39cb4284602250383b4b2574b501d471402ff96
Philippe Peelman <philippe.peelman@gmail.com>
2019-04-12 10:12:24 +0200

    Upgraded to newest version webpack & babel + replaced Express by webpack-dev-server as dev server

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//import numeral from "numeral";
+import _ from "lodash.capitalize";
+
+import style from "./main.css";
+
+//const courseValue = numeral(1000).format("$0,0.00");
+//console.log(`I would pay ${courseValue} for this amazing course!`);
+
+console.log(_.capitalize("philippe"));
