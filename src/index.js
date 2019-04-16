61bc0e73cb04ad3356ca0f6742499e1c977933f0
Philippe Peelman <philippe.peelman@gmail.com>
2019-04-16 16:38:58 +0200

    Add eslint-loader to webpack config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // import numeral from "numeral";
-import capitalize from "lodash.capitalize";
+import capitalize from 'lodash.capitalize';
 
-import style from "./main.css";
+import './main.css';
 
 // const courseValue = numeral(1000).format("$0,0.00");
 // console.log(`I would pay ${courseValue} for this amazing course!`);
 
-console.log(capitalize("philippe"));
+const name = capitalize('philippe');
+alert(name); //eslint-disable-line
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,47 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  devtool: "inline-source-map",
-  entry: [path.resolve(__dirname, "src/index")],
-  target: "web",
+  devtool: 'inline-source-map',
+  entry: [path.resolve(__dirname, 'src/index')],
+  target: 'web',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
-    filename: "main.js"
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'main.js',
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
+      template: './src/index.html',
+      filename: './index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        use: ['babel-loader', 'eslint-loader'],
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-      }
-    ]
-  }
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
 };
